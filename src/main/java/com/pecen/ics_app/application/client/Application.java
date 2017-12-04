4bc6751388260ac3bdaf356f58ebe21d458f9106
pechen <elenabarmina@yandex.ru>
2017-12-04 23:35:03 +0500

    Initial commit

---
+++ src/main/java/com/pecen/ics_app/application/client/Application.java
@@ -0,0 +1,21 @@
+package com.pecen.ics_app.application.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>
+ */
+public class Application implements EntryPoint {
+
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+
+    }
+}
